5cfbbfb5bb84b3b6d642c42f75ca4e2b5ef707fc
Jonathan Chiang <jc91765@gmail.com>
2022-04-22 19:06:26 -0700

    revised starting test data

--- database/data.sql
+++ database/data.sql
@@ -59,47 +59,7 @@ insert into "tags"
 insert into "recipes"
   ("userId", "name", "instructions", "notes")
   values
-    (1, 'Guacamole', 'Combine ingredients in bowl, add salt and pepper to taste.', 'test recipe');
-
-insert into "ingredients"
-  ("name")
-  values
-    ('avocadoes'),
-    ('serrano chile'),
-    ('cilantro'),
-    ('salt'),
-    ('limes');
-
-insert into "pictures"
-  ("url", "recipeId")
-  values
-    ('https://i.imgur.com/toSH7qV.jpeg', 2);
-
-insert into "recipeIngredients"
-  ("recipeId", "ingredientId", "amount", "preparation")
-  values
-  (2, 11, '4', ''),
-  (2, 5, '1', 'diced'),
-  (2, 12, '1', 'diced'),
-  (2, 13, '1/2 cup', ''),
-  (2, 14, '', 'to taste'),
-  (2, 15, '2', '');
-
-insert into "tags"
-  ("name")
-  values
-  ('vegan');
-
-  insert into "recipeTags"
-    ("recipeId", "tagId")
-    values
-    (2, 4);
-
-
-insert into "recipes"
-  ("userId", "name", "instructions", "notes")
-  values
-    (1, 'Old Fashioned', 'Combine ingredients in bowl, add salt and pepper to taste.', 'test recipe');
+    (1, 'Guacamole', E'Split avocadoes in half, discard pits, scoop out avocadoes into bowl with onions, cilantro, and chiles\nJuice 2 limes into bowl\nFold ingredients together in bowl, add salt to taste.', 'test recipe from Serious Eats');
 
 insert into "ingredients"
   ("name")
